2fbb2ab5eefd4688524a73dfeef949749d2b8a3a
Ismael Yahyaoui Racine <iyahoui-@c2r3p2.42quebec.com>
2022-02-07 18:13:12 -0500

    school -> macbook

--- open_window.c
+++ open_window.c
@@ -11,14 +11,23 @@ int	main(void)
 	int		size_y;
 
 	mlx_ptr = mlx_init();
-	size_x = 100;
-	size_y = 100;
+	if (!mlx_ptr)
+		return (0);
+	size_x = 800;
+	size_y = 800;
 	win_ptr = mlx_new_window(mlx_ptr, size_x, size_y, title);
+	if (!win_ptr)
+	{
+		free (mlx_ptr);
+		mlx_ptr = NULL;
+		return (0);
+	}
 	mlx_loop(mlx_ptr);
-	sleep(3);
+	sleep(1);
 	mlx_clear_window(mlx_ptr, win_ptr);
-	sleep(3);
+	sleep(1);
 	mlx_destroy_window(mlx_ptr, win_ptr);
+	mlx_destroy_display(mlx_ptr);
 
 	free(mlx_ptr);
 	return (0);
